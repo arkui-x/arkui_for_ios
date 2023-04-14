8bd5ad144dac2ee6bec48dab9698584eb481ce6f
qugr <quguiren@huawei.com>
2023-04-14 14:28:41 +0000

    build error
    Signed-off-by: qugr <quguiren@huawei.com>

--- stage/uicontent/ace_container_sg.cpp
+++ stage/uicontent/ace_container_sg.cpp
@@ -15,8 +15,8 @@
 
 #include "adapter/ios/stage/uicontent/ace_container_sg.h"
 
-#include "adapter/ios/entrance/ace_application_info_impl.h"
-#include "adapter/ios/entrance/ace_plaform_plugin.h"
+#include "adapter/ios/entrance/ace_application_info_impl.h"t
+#include "adapter/ios/entrance/ace_platform_plugin.h"
 #include "adapter/ios/osal/file_asset_provider.h"
 #include "adapter/ios/stage/uicontent/ace_view_sg.h"
 #include "base/log/ace_trace.h"
