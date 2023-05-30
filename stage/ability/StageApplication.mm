4fb2a10c483e5662bd13ae544f48c38225d291f6
liu_wei <liuwei742@huawei.com>
2023-05-30 11:19:03 +0800

    fix bug of findTopViewController
    Signed-off-by: liu_wei <liuwei742@huawei.com>
    Change-Id: I5fceedd36c43b9a383e9b70b6b4c03be636687bf

--- stage/ability/StageApplication.mm
+++ stage/ability/StageApplication.mm
@@ -132,15 +132,22 @@ using AppMain = OHOS::AbilityRuntime::Platform::AppMain;
     return (StageViewController *)[self findTopViewController:viewController];
 }
 
-+ (UIViewController *)findTopViewController:(UIViewController*)vc {
-    if ([vc isKindOfClass:[UINavigationController class]]) {
-        UINavigationController *svc = (UINavigationController*)vc;
-        if (svc.viewControllers.count > 0) {
-            return [self findTopViewController:svc.topViewController];
++ (UIViewController *)findTopViewController:(UIViewController*)topViewController {
+    while (true) {
+        if (topViewController.presentedViewController) {
+            topViewController = topViewController.presentedViewController;
+        } else if ([topViewController isKindOfClass:[UINavigationController class]]
+                    && [(UINavigationController*)topViewController topViewController]) {
+            topViewController = [(UINavigationController *)topViewController topViewController];
+        } else if ([topViewController isKindOfClass:[UITabBarController class]]) {
+            UITabBarController *tab = (UITabBarController *)topViewController;
+            topViewController = tab.selectedViewController;
+        } else {
+            break;
         }
-        return vc;
     }
-    return vc;
+    return topViewController;
 }
+
 
 @end
