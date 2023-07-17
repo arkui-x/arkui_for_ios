526f8d6d5a0468f75ccbabd796d48658c66276da
lixingchi1 <lixingchi1@huawei.com>
2023-07-17 16:11:29 +0800

    ios update to tag 0717
    Signed-off-by: lixingchi1 <lixingchi1@huawei.com>

--- osal/system_properties.cpp
+++ osal/system_properties.cpp
@@ -58,7 +58,6 @@ bool SystemProperties::debugEnabled_ = false;
 bool SystemProperties::debugBoundaryEnabled_ = false;
 bool SystemProperties::extSurfaceEnabled_ = true;
 uint32_t SystemProperties::dumpFrameCount_ = 0;
-PerformancePtr SystemProperties::performanceProps_ = nullptr;
 
 void SystemProperties::InitDeviceType(DeviceType type)
 {
@@ -166,18 +165,6 @@ bool SystemProperties::GetIsUseMemoryMonitor()
     return false;
 }
 
-void SystemProperties::InitPerformanceParameters() {}
-
-bool SystemProperties::IsPerformanceCheckEnabled()
-{
-    return false;
-}
-
-int32_t SystemProperties::GetPerformanceParameterWithType(PerformanceParameterType type)
-{
-    return 0;
-}
-
 bool SystemProperties::IsFormAnimationLimited()
 {
     return false;
@@ -192,5 +179,7 @@ bool SystemProperties::IsSceneBoardEnabled()
 {
     return false;
 }
+
+void SystemProperties::GetAppBarInfo(std::string& bundleName, std::string& abilityName) {}
 
 } // namespace OHOS::Ace
