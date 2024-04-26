79c8a3fb7a696bbe11f9962b877b741bbf4d4383
zhangqing <zhangqing50@h-partners.com>
2024-04-26 14:44:10 +0800

    arkui-x update master OH to weekly_20240415 and arkui-x fix build bug
    Change-Id: I890f6a0e6819479831734f54e2b818cb1fec5d99

--- osal/system_properties.cpp
+++ osal/system_properties.cpp
@@ -255,4 +255,9 @@ float SystemProperties::GetDefaultResolution()
     return 1.0f;
 }
 
+std::string SystemProperties::GetAtomicServiceBundleName()
+{
+    return UNDEFINED_PARAM;
+}
+
 } // namespace OHOS::Ace
