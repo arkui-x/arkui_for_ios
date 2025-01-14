3a48f789e783d6e79f0f6532de6223bc8c8e7754
quguiren <quguiren@huawei.com>
2025-01-14 16:05:44 +0800

    跨平台适配onConfigurationUpdate
    Signed-off-by: quguiren <quguiren@huawei.com>

--- stage/ability/StageConfigurationManager.h
+++ stage/ability/StageConfigurationManager.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -27,9 +27,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (instancetype)configurationManager;
 
-- (void)setDirection:(UIDeviceOrientation)direction;
+- (void)setDirection:(UIInterfaceOrientation)direction;
 
-- (void)directionUpdate:(UIDeviceOrientation)direction;
+- (void)directionUpdate:(UIInterfaceOrientation)direction;
 
 - (void)setColorMode:(UIUserInterfaceStyle)colorMode;
 
