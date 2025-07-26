afe8119e1c13b0e02d9cf7c1ed9212f5822fc029
yanjunliang@h-partners.com <yanjunliang@h-partners.com>
2025-07-26 14:48:11 +0800

    修改startAbility方法中canOpenURL方法在子线程调用的问题
    Signed-off-by: yanjunliang@h-partners.com <yanjunliang@h-partners.com>

--- stage/ability/ability_context_adapter.mm
+++ stage/ability/ability_context_adapter.mm
@@ -152,15 +152,14 @@ int32_t AbilityContextAdapter::StartAbility(const std::string& instanceName, con
         }
         components.queryItems = queryItems;
         NSURL *appUrl = components.URL;
-
-        if ([[UIApplication sharedApplication] canOpenURL:appUrl]) {
-            dispatch_main_async_safe(^{
+        dispatch_main_async_safe(^{
+            if ([[UIApplication sharedApplication] canOpenURL:appUrl]) {
+                NSLog(@"yanTestNow canOpenURL in %@",appUrl.absoluteString);
                 [[UIApplication sharedApplication] openURL:appUrl options: @{} completionHandler: ^(BOOL success) {}];
-            });
-        } else {
-            NSLog(@"startAbility failed, can't open app");
-            return AAFwk::RESOLVE_ABILITY_ERR;
-        }
+            } else {
+                NSLog(@"startAbility failed, can't open app");
+            }
+        });
     }
     return ERR_OK;
 }
