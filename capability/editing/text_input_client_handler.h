2cdb994477809bb1a377c50d542e5abca40fbb30
quguiren <quguiren1@huawei-partners.com>
2025-09-26 09:52:38 +0800

    软键盘拉起关闭优化
    Signed-off-by: quguiren <quguiren1@huawei-partners.com>

--- capability/editing/text_input_client_handler.h
+++ capability/editing/text_input_client_handler.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -36,6 +36,14 @@ public:
     {
         // RefPtr has already overload operator == with raw pointer.
         return currentConnection_ == connection;
+    }
+
+    TextInputConnection* GetCurrentConnection()
+    {
+        if (currentConnection_) {
+            return AceType::RawPtr(currentConnection_);
+        }
+        return nullptr;
     }
 
     void UpdateEditingValue(
