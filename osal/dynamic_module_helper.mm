f0064f97d60b7c749c31b5feb26dc77ba5d22362
quguiren <quguiren1@huawei-partners.com>
2026-01-13 14:10:57 +0800

    组件化编译gauge
    Signed-off-by: quguiren <quguiren1@huawei-partners.com>

---
+++ osal/dynamic_module_helper.mm
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "core/common/dynamic_module_helper.h"
+
+#import <Foundation/Foundation.h>
+
+#include <dlfcn.h>
+#include <memory>
+
+#include "base/utils/utils.h"
+#include "compatible/components/component_loader.h"
+#include "interfaces/inner_api/ace/utils.h"
+
+namespace OHOS::Ace {
+namespace {
+const std::string DYNAMIC_MODULE_LIB_PREFIX = "libarkui_";
+static NSString* DYNAMIC_MODULE_LIB_POSTFIX = @".dylib";
+static NSString* FRAMEWORK_TYPE = @"framework";
+} // namespace
+DynamicModuleHelper& DynamicModuleHelper::GetInstance()
+{
+    static DynamicModuleHelper instance;
+    return instance;
+}
+
+std::unique_ptr<ComponentLoader> DynamicModuleHelper::GetLoaderByName(const char* name)
+{
+    return nullptr;
+}
+
+DynamicModule* DynamicModuleHelper::GetDynamicModule(const std::string& name)
+{
+    // Double-checked locking pattern for better performance
+    {
+        std::lock_guard<std::mutex> lock(moduleMapMutex_);
+        auto iter = moduleMap_.find(name);
+        if (iter != moduleMap_.end()) {
+            return iter->second.get();
+        }
+    }
+
+    // Load module without holding the lock (dlopen/dlsym may be slow)
+    std::string moduleNameStr = DYNAMIC_MODULE_LIB_PREFIX + name;
+    NSString* moduleName = [NSString stringWithUTF8String:moduleNameStr.c_str()];
+    NSString* frameworkPath = [[NSBundle mainBundle] pathForResource:moduleName ofType:FRAMEWORK_TYPE];
+    NSString* dylibPath = [frameworkPath stringByAppendingPathComponent:[moduleName stringByAppendingString:DYNAMIC_MODULE_LIB_POSTFIX]];
+    const char* libName = [dylibPath UTF8String];
+    auto* handle = dlopen(libName, RTLD_NOLOAD);
+    LOGI("First load %{public}s nativeModule start", name.c_str());
+    if (handle == nullptr) {
+        LOGE("Failed to load dynamic module library: %{public}s, error: %{public}s", name.c_str(), dlerror());
+        return nullptr;
+    }
+    auto* createSym = reinterpret_cast<DynamicModuleCreateFunc>(dlsym(handle, DYNAMIC_MODULE_CREATE));
+    if (createSym == nullptr) {
+        LOGE("Failed to find symbol in library %{public}s, error: %{public}s", name.c_str(), dlerror());
+        dlclose(handle);
+        return nullptr;
+    }
+    DynamicModule* module = createSym();
+    if (module == nullptr) {
+        LOGE("Failed to create DynamicModule instance from library %{public}s", name.c_str());
+        dlclose(handle);
+        return nullptr;
+    }
+    LOGI("First load %{public}s nativeModule finish", name.c_str());
+
+    // Lock again to insert into map
+    {
+        std::lock_guard<std::mutex> lock(moduleMapMutex_);
+        // Check again in case another thread already loaded it
+        auto iter = moduleMap_.find(name);
+        if (iter != moduleMap_.end()) {
+            // Another thread already loaded it, use that one
+            delete module;
+            dlclose(handle);
+            return iter->second.get();
+        }
+        moduleMap_.emplace(name, std::unique_ptr<DynamicModule>(module));
+        return module;
+    }
+}
+
+} // namespace OHOS::Ace
